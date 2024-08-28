0ec31dc8cad692b0d2c27962abfd366c45900fa0
AbelMuro <abelmuro93@gmail.com>
2024-08-27 17:25:18 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const S3 = require('./SDK/AWS');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();                                      
+const port = 4000;
+
+app.use(cors({
+    origin: '*', 
+    optionsSuccessStatus: 200 
+}));
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+    const formData = req.body;
+    const firstName = formData.firstName;
+    const lastName = formData.lastName;
+    S3.PutObject(`${firstName} ${lastName}`, formData);
+    res.send('Form has been uploaded');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});                                         
