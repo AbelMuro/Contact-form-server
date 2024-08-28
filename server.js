b1a261ed1b729d764073d753a93321f45f629c18
AbelMuro <abelmuro93@gmail.com>
2024-08-28 14:13:34 -0700

    commit

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ app.post('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running`);
 });                                         
