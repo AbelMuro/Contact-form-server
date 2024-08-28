2a91ae4b4fa317d1ff997277b2069f5ce18cabec
AbelMuro <abelmuro93@gmail.com>
2024-08-28 15:14:09 -0700

    changed port used by the server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();                                      
-const port = 4000;
+const port = 80;
 
 app.use(cors({
     origin: '*', 
