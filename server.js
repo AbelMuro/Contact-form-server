5156a924c7fd24671e987603bac61a4f3a40a16a
AbelMuro <abelmuro93@gmail.com>
2024-08-30 15:32:56 -0700

    commmit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();                                      
-const port = 80;
+const port = 443;
 
 app.use(cors({
     origin: '*', 
