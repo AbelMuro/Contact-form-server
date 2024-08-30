47587b0e1d185fed31048265a40263e7206c870c
AbelMuro <abelmuro93@gmail.com>
2024-08-30 15:16:18 -0700

    updated the port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();                                      
-const port = 443;
+const port = 80;
 
 app.use(cors({
     origin: '*', 
