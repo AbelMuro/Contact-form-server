14d1ecc324b6b6c5b39b47d8c664b8b487cfd7d1
abelmuro <abelmuro93@gmail.com>
2024-08-31 11:37:09 -0700

    commit

--- server.js
+++ server.js
@@ -35,10 +35,10 @@ app.post('/webhook', (req, res) => {
             }
             console.log(`stdout: ${stdout}`);
             console.error(`stderr: ${stderr}`);
-            res.status(200).send('Changes pulled successfully');
+            res.send('Changes pulled successfully');
         });
     } else {
-        res.status(200).send('Not a push event to the main branch');
+        res.send('Not a push event to the main branch');
     }
 });
 
