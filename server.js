6fa440d97f1418b6f492689757c044845fbef545
abelmuro <abelmuro93@gmail.com>
2024-08-31 11:44:09 -0700

    commit

--- server.js
+++ server.js
@@ -14,16 +14,17 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(express.json());
 
+
+
 app.post('/', (req, res) => {
     const formData = req.body;
-    if(!formData.firstName) return;
     const firstName = formData.firstName;
     const lastName = formData.lastName;
     S3.PutObject(`${firstName} ${lastName}`, formData);
     res.send('Form has been uploaded');
 });
 
-app.post('/', (req, res) => {
+app.post('/webhook', (req, res) => {
     const payload = req.body;
 
     // Check if the event is a push event
