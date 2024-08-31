fe44cef56de454586e2e88399700d971b93e8c07
abelmuro <abelmuro93@gmail.com>
2024-08-31 10:58:33 -0700

    commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const S3 = require('./SDK/AWS');
+const { exec } = require('child_process');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -17,7 +18,27 @@ app.post('/', (req, res) => {
     const lastName = formData.lastName;
     S3.PutObject(`${firstName} ${lastName}`, formData);
     res.send('Form has been uploaded');
-})
+});
+
+app.post('/webhook', (req, res) => {
+    const payload = req.body;
+
+    // Check if the event is a push event
+    if (payload.ref === 'refs/heads/main') {
+        // Pull the latest changes from the repository
+        exec('cd Contact-form-server && git pull', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error pulling changes: ${error.message}`);
+                return res.status(500).send('Error pulling changes');
+            }
+            console.log(`stdout: ${stdout}`);
+            console.error(`stderr: ${stderr}`);
+            res.status(200).send('Changes pulled successfully');
+        });
+    } else {
+        res.status(200).send('Not a push event to the main branch');
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on this port ${port}`);
