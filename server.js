90e0b724ea626b5e98c1ec61a69db1371a2142e5
abelmuro <abelmuro93@gmail.com>
2024-08-31 11:52:08 -0700

    commit

--- server.js
+++ server.js
@@ -44,5 +44,5 @@ app.post('/webhook', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on this port ${port}`);
+    console.log(`Server is running on this awesome port ${port}`);
 });                                         
