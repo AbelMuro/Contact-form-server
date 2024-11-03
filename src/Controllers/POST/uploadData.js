f7d4f7011801bb4f36afb5d16854578d29bcfff6
abelmuro <abelmuro93@gmail.com>
2024-11-03 15:22:28 -0800

    commit

--- src/Controllers/POST/uploadData.js
+++ src/Controllers/POST/uploadData.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const router = express.Router();
 const {Enquiry} = require('../../Models/Models.js');
-const ObjectId = mongoose.Types.ObjectId;
 
 
 router.post('/upload_data', (req, res) => {
