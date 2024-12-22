1dd11f8e0e2171146149908c516c5c2f12628871
abelmuro <abelmuro93@gmail.com>
2024-12-21 23:46:43 -0800

    commit

--- functions/app.js
+++ functions/app.js
@@ -1,5 +1,5 @@
 const serverless = require('serverless-http'); 
-const app = require('../src/index.js'); 		//make sure you export the app module from the index.js
+const app = require('../src/index.js'); 	
 const connectDB = require('../src/Database/db.js');
 
 const handler = serverless(app);  		       
